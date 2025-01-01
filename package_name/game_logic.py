c8342addad6feaa1b7fb76c75e24a5a24b6fc3b6
viklundechka <vikagusak2005@gmail.com>
2025-01-01 23:03:52 +0300

    game_logic.py

---
+++ package_name/game_logic.py
@@ -0,0 +1,116 @@
+from itertools import cycle
+from typing import NamedTuple
+
+class Player(NamedTuple):
+    """Представляет игрока в игре.
+    Атрибуты:
+        label: Символ игрока (например, 'X' или 'O').
+        color: Цвет символа игрока.
+    """
+    label: str
+    color: str
+
+class Move(NamedTuple):
+    """Представляет ход игрока.
+    Атрибуты:
+        row: Номер строки на игровом поле.
+        col: Номер столбца на игровом поле.
+        label: Символ, которым обозначен ход.
+    """
+    row: int
+    col: int
+    label: str = ""
+
+class TicTacToeGame:
+    """Класс, реализующий логику игры в крестики-нолики."""
+
+    def __init__(self, players, board_size=3):
+        """Инициализация игры.
+        Аргументы:
+            players: Список игроков.
+            board_size: Размер игрового поля.
+        """
+        self._players = cycle(players)
+        self.board_size = board_size
+        self.current_player = next(self._players)
+        self.winner_combo = []
+        self._current_moves = []
+        self._has_winner = False
+        self._winning_combos = []
+        self._setup_board()
+
+    def _setup_board(self):
+        """Создать начальное состояние игрового поля."""
+        self._current_moves = [
+            [Move(row, col) for col in range(self.board_size)]
+            for row in range(self.board_size)
+        ]
+        self._winning_combos = self._get_winning_combos()
+
+    def _get_winning_combos(self):
+        """Сформировать комбинации выигрышных ходов."""
+        rows = [
+            [(move.row, move.col) for move in row]
+            for row in self._current_moves
+        ]
+        columns = [list(col) for col in zip(*rows)]
+        first_diagonal = [row[i] for i, row in enumerate(rows)]
+        second_diagonal = [col[j] for j, col in enumerate(reversed(columns))]
+        return rows + columns + [first_diagonal, second_diagonal]
+
+    def toggle_player(self):
+        """Переключить текущего игрока."""
+        self.current_player = next(self._players)
+
+    def is_valid_move(self, move):
+        """Проверить, допустим ли ход.
+        Аргументы:
+            move: Ход игрока.
+        Возвращает:
+            True, если ход допустим, иначе False.
+        """
+        row, col = move.row, move.col
+        move_was_not_played = self._current_moves[row][col].label == ""
+        no_winner = not self._has_winner
+        return no_winner and move_was_not_played
+
+    def process_move(self, move):
+        """Обработать ход игрока и проверить, есть ли победа.
+        Аргументы:
+            move: Ход игрока.
+        """
+        row, col = move.row, move.col
+        self._current_moves[row][col] = move
+        for combo in self._winning_combos:
+            results = set(self._current_moves[n][m].label for n, m in combo)
+            is_win = (len(results) == 1) and ("" not in results)
+            if is_win:
+                self._has_winner = True
+                self.winner_combo = combo
+                break
+
+    def has_winner(self):
+        """Проверить, есть ли победитель.
+        Возвращает:
+            True, если есть победитель, иначе False.
+        """
+        return self._has_winner
+
+    def is_tied(self):
+        """Проверить, завершилась ли игра вничью.
+        Возвращает:
+            True, если ничья, иначе False.
+        """
+        no_winner = not self._has_winner
+        played_moves = (
+            move.label for row in self._current_moves for move in row
+        )
+        return no_winner and all(played_moves)
+
+    def reset_game(self):
+        """Сбросить состояние игры для новой партии."""
+        for row, row_content in enumerate(self._current_moves):
+            for col, _ in enumerate(row_content):
+                row_content[col] = Move(row, col)
+        self._has_winner = False
+        self.winner_combo = []
